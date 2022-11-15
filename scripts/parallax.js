94442a07a06e56066214e578aad774dae5954769
BladedShip <67158370+BladedShip@users.noreply.github.com>
2022-11-15 16:56:38 +0530

    Update parallax.js

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -1,7 +1,8 @@
 let bgimg = document.getElementById("para");
 
 window.addEventListener('scroll',() =>{
-    let ass=window.scrollY;
 
+    let ass=window.scrollY;
     bgimg.style.top=0.5*ass+'px';
+    
 });
