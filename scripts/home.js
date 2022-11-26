cfaf10556ac271f71055a14db033d5af66af2859
Mayank D <mayankdj7001@gmail.com>
2022-11-26 14:37:27 +0530

    myree

--- scripts/home.js
+++ scripts/home.js
@@ -5,8 +5,10 @@ const glay =document.getElementById('sm');
 const rf = document.getElementById('rfb');
 const burgmen = document.getElementById("burmnu");
 const togb = document.getElementById("toggb");
-var flag = false;
+const togas = document.getElementById("aso");
+const togi = document.getElementById("op");
 let x = window.matchMedia("(min-width: 500px)");
+var i=1 ;
 
 window.addEventListener('scroll',() =>{
     let ass = window.scrollY;
@@ -20,8 +22,8 @@ window.addEventListener('scroll',() =>{
     if(window.pageYOffset>500)
     {   
         shp.style.fontSize='4rem';
-        shp.style.transition="1s";
-        rf.style.transition="1s";
+        shp.style.transition=".5s";
+        rf.style.transition=".5s";
         shp.style.marginLeft="2rem";
     }
 }
@@ -30,8 +32,8 @@ else
     if(window.pageYOffset>300)
     {   
         shp.style.fontSize='2rem';
-        shp.style.transition="1s";
-        rf.style.transition="1s";
+        shp.style.transition=".5s";
+        rf.style.transition=".5s";
         shp.style.marginLeft="2rem";
     }
 }
@@ -43,8 +45,8 @@ window.addEventListener('scroll',() =>{
     {
         shp.style.fontSize="2rem";
         shp.style.marginLeft="1rem";
-        shp.style.transition="1s";
-        rf.style.transition="1s";
+        shp.style.transition=".5s";
+        rf.style.transition=".5s";
     }
 }
     else
@@ -53,8 +55,8 @@ window.addEventListener('scroll',() =>{
         {
             shp.style.fontSize="1rem";
             shp.style.marginLeft="1rem";
-            shp.style.transition="1s";
-            rf.style.transition="1s";
+            shp.style.transition=".5s";
+            rf.style.transition=".5s";
         }
     }
 });
@@ -63,3 +65,12 @@ togb.addEventListener('click',() =>{
     burgmen.classList.toggle('active');
 }
 )
+change=()=>{
+    i=i%5+1;
+    togi.style.transition="300ms"
+    togi.style.backgroundImage="url('RES/bg/mbg"+i+".jpg')";
+    setTimeout(change, 3000);
+    console.log("PUNDAE "+i);
+    
+};
+change();
